e00df7dab4136886512df35fb5c967c1af4aeb14
JayMon0327 <124110982+JayMon0327@users.noreply.github.com>
2023-02-02 17:44:13 +0900

    4일차

---
+++ Day04/out.js
@@ -0,0 +1,27 @@
+function 빨래하다() {
+    console.log("1.먼지를 털다.")
+    console.log("2.물에 담군다.")
+    console.log("3.비누를 바른다.")
+    console.log("4.오물조물 비빈다.")
+    console.log("5.물에 여러번 헹군다.")
+    console.log("6.짠다.")
+    console.log("7.널다.")
+}
+
+
+function 청소하다() {
+    alert("1.보이는걸 치운다")
+    alert("2.닦는다")
+}
+
+function 여행하다() {
+    prompt("1.어디를 가고 싶으신가요")
+    prompt("2.어느 교통편을 타고 싶으신가요")
+
+}
+
+function 쉬다() {
+    alert("50분이 되면")
+    alert("나는 쉰다")
+}
+
