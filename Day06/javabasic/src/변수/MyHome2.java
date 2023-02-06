048c600a8dfa328b279b320762e618d882651d69
JayMon0327 <124110982+JayMon0327@users.noreply.github.com>
2023-02-06 18:37:03 +0900

    6일차 자바 시작

---
+++ Day06/javabasic/src/변수/MyHome2.java
@@ -0,0 +1,10 @@
+package 변수;
+
+public class MyHome2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day06/javabasic/src/확인문제/문제풀이3.java
@@ -0,0 +1,20 @@
+package 확인문제;
+
+
+
+
+public class 문제풀이3 {
+
+	public static void main(String[] args) {
+		char myblood = 'o';
+		boolean bab = true;
+		int player = 4500000;
+		String name = "이후성";
+		
+		
+		
+				
+
+	}
+
+}
